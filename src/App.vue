7b746096b2f9702f6d5a167ebec777863f743fd8
Matthew <leonar1509@gmail.com>
2023-09-20 19:27:15 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+#app {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
